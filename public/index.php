ae95dc242a7898719264a1d472115be671e0d196
Vitos <vitosnev@mail.ru>
2018-02-16 23:44:56 +0300

    второй пункт

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<title>Тест</title>
 	<link href="/css/layout.css" media="screen" rel="stylesheet" type="text/css" > 
+	<link href="http://bootstrap-3.ru/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<h2>Новости Ленты.ру</h2>
@@ -15,12 +16,12 @@
 	      <th>Изображение</th>
 	      <th>Название</th>
 	      <th>Краткое описание</th>
-	      <th>Ссылка</th>
+	      <th>Ccылка</th>
 	    <tr>
 	  </thead>
 	  <tbody data-bind="foreach: news">
 	    <tr data-bind="css: {selected: $data == $root.chosenRowId()},
-	                   click: $root.click">
+	                   click: {single: $root.select, double: $root.viewNew}">
 	      <td data-bind="text: time"></td>
 	      <td>
 	        <img data-bind="attr: {src: picture_path}" width="150">
@@ -29,12 +30,26 @@
 	      <td data-bind="text: small_text"></td>
 	      <td>
 	        <a data-bind="attr: {href: link}" target="_blank">
-	          Cсылка
+	          Lenta.ru
 	        </a>
 	      </td>
 	    </tr>
 	  </tbody>
 	</table>
+	
+	<button class="btn btn-lg btn-success"> Обновить </button>
+	
+	<!-- модальное окно -->
+	<div id="modal" data-bind="with: newinmodal">
+	  <span id="modal_close" onclick="closeWin()">X</span>
+	  <div class="time" data-bind="text: time"></div>
+	  <div class="image">
+	     <img data-bind="attr: {src: picture_path}" width="450">
+	  </div>
+	  <div class="text" data-bind="text: small_text"></div>
+	</div>
+  <div id="overlay" onclick="closeWin()"></div>
+  	
 	<script src="http://knockoutjs.com/downloads/knockout-3.4.2.js"></script>
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script src="/js/index.js"></script>
--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,72 @@
+ko.bindingHandlers.click = {
+  init: function(element, valueAccessor, allBindingsAccessor, viewModel, context) {
+    var accessor = valueAccessor();
+    var clicks = 0;
+    var timeout = 200;
+
+    $(element).click(function(event) {
+      if(typeof(accessor) === 'object') {
+        var single = accessor.single;
+        var double = accessor.double;
+        clicks++;
+        if (clicks === 1) {
+            setTimeout(function() {
+                if(clicks === 1) {
+                    single.call(viewModel, context.$data, event);
+                } else {
+                    double.call(viewModel, context.$data, event);
+                }
+                clicks = 0;
+            }, timeout);
+        }
+      } else {
+        accessor.call(viewModel, context.$data, event);
+      }
+    });
+  }
+};
+
 function NewsViewModel(){
   var self = this;
   
   self.news = ko.observable();
   self.chosenRowId = ko.observable();
+  self.newinmodal = ko.observable();
     
   $.getJSON("/parser.php", function(data){
     self.news(data);
   })
   
-  self.click = function(row){
+  self.select = function(row){
     self.chosenRowId(row);
   }
-   
+  
+  self.viewNew = function(row){
+    $.getJSON('/getNews.php', {path: row.link}, function(data){
+      self.newinmodal(data);
+      
+      viewNew();
+    });        
+  }
 }
 
 ko.applyBindings(new NewsViewModel());
+
+viewNew = function(){
+  $('#overlay').fadeIn(400, function(){
+    $('#modal').css('display', 'block')
+               .animate({opacity: 1, top: '50%'}, 200);
+  });
+}
+
+
+closeWin = function(){
+  $('#modal')
+  .animate({opacity: 0, top: '45%'}, 200,
+    function(){
+      $(this).css('display','none');
+      $('#overlay').fadeOut(400);
+    }
+  );
+}
+
